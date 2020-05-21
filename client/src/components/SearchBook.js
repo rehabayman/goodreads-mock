06726ac825fd701517e2e75c61d1fa9f3d802f32
basmmohamed <basmasalah2017@gmail.com>
2020-05-21 05:12:02 +0200

    search done

--- client/src/components/SearchBook.js
+++ client/src/components/SearchBook.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react'
 import './starStyle.css'
 import axios from 'axios';
 import authHeader from '../services/auth-header'
+// import "netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css";
 import SearchResult from './searchresults';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // import { browserHistory } from 'react-router';
@@ -16,7 +17,7 @@ const SearchBook=(props)=>  {
     let [input, setInput] = useState('');
     const [books, setBooks]=useState([]);  
     const [filteredBooks,setfilteredBooks]=useState([]);
-    const [show,setShow]=useState(false);
+    let [show,setShow]=useState(false);
     let currentBooks= [];
     const [showBooks,setShowBooks]=useState(2);
 
@@ -98,28 +99,66 @@ const SearchBook=(props)=>  {
         
     }
     const dropdownContent = {
-        // display: "block",
         display: show ? "block" : "none",
         position: "absolute",
         backgroundColor: "#f1f1f1",
         minWidth: "260px",
+        maxWidth: "265px",
         boxShadow: "0px 8px 16px 0px rgba(0,0,0,0.2)",
         // zIndex: 1,
       }
+      const searchItem={
+            border: "0",
+            padding: "10px",
+            minHeight: "25px",
+            backgroundColor: "#fff",
+            boxShadow: "none",
+            borderRadius: "3px",
+            position: "relative",
+            maxHeight: "60px",
+            borderBottom: "2px solid #ccc",
+            transition: "maxHeight 0.5s ease",
+          
+      }
+     const row={
+          display:"flex",
+      }
+     const title={
+
+        margin: "0 15px 5px",
+        color: "#333",
+        }
+     const author={ 
+        margin: "0 15px 5px",
+        fontSize: "12px",
+        color: "#333",
+    }
  
 
     return (
         <div className="dropdown" style={dropdownStyle}>
             
             <form onSubmit={handleSubmit}>
-                <input type="text" className="input" value={input} onClick={e=>{setShow(true);console.log(filteredBooks);}
-                } style={inputStyle} onChange={e => {handleChange(e);}} placeholder="Search..." />
+                <input type="text" className="input" value={input} onFocus={e=>{!show?setShow(true):setShow(false);console.log(filteredBooks);}
+                }onBlur={e=>{show?setShow(false):setShow(true)}} style={inputStyle} onChange={e => {handleChange(e);}} placeholder="Search..." />
             </form >
-            <div onClick={e=>{setShow(false);setfilteredBooks([])}} >
+            <div>
             <datalist className="dropdownContent" style={dropdownContent} >
             {filteredBooks.length ? filteredBooks.slice(0,showBooks).map((i,index) => 
                    <li key={index}>
-                   <Link to={`/books/${i._id}`}  >{i.name} <br/><span>by {i.author.firstName} {i.author.lastName}</span></Link><br/>
+                       <div className="searchItem" style={searchItem}>
+                            <div className="row">
+                                <Link to={`/books/${i._id}`} style={row}>
+                                    <div className="col-xs-6 col-sm-3 col-md-3 col-lg-2">
+                                        <img className="img-responsive" src="https://via.placeholder.com/50x50" alt=""/>
+                                    </div>
+                                    <div className="col-xs-6 col-sm-9 col-md-9 col-lg-10">
+                                        <h5 style={title}>{i.name} </h5>
+                                        <p style={author}>by {i.author.firstName} {i.author.lastName}</p>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
                    </li>
                    
                 ):
@@ -137,10 +176,7 @@ const SearchBook=(props)=>  {
             </datalist>
             </div>
 
-            <div onClick={e=>{setShow(false);setfilteredBooks([])}}>
-          
-            
-            </div>
+  
         </div>
     );
     
